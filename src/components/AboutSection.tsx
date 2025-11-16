646ff56ee63cf0c426471041ecea443ec26e7a1b
Randomlystrange <170492684+Randomlystrange@users.noreply.github.com>
2025-11-16 13:00:05 +0530

    d

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -17,7 +17,7 @@ const features = [
 
 const AboutSection = () => {
   return (
-    <section id="about" className="min-h-screen py-20 px-4 md:px-8 relative">
+    <section id="about" className="min-h-screen pt-20 pb-16 px-4 md:px-8 relative">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
